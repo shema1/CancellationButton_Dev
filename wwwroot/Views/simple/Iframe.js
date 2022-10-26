de50a1e787faff7a860bed0301f1a8555fc34891
Mykola Shymechko <m.shymechko@brainence.com>
2022-10-26 14:29:28 +0300

    first commit

---
+++ wwwroot/Views/simple/Iframe.js
@@ -0,0 +1,9 @@
+var IframeView = function ($scope) {
+
+  const orderInfo = JSON.parse(localStorage.getItem('move_cancel_selected_order_info'));
+
+  var appFrame = document.getElementById("appFrame");
+
+  appFrame.src = `http://localhost:2000/?token=6c324e9a-bf7d-adb4-900d-eb4420a1e02d&orderId=${orderInfo.OrderId}&email=akornytskyi@brainence.com`
+
+};
---
+++ wwwroot/index.js
@@ -0,0 +1,71 @@
+"use strict";
+
+
+define(function (require) {
+
+  const wind = require('core/Window');
+
+  const placeholderManager = require("core/placeholderManager");
+
+  var LookupPlaceholder = function ($scope, $element, controlService, openOrdersService, $http, $timeout, $compile) {
+    this.getItems = () => {
+
+      var items = [{
+        text: "Move/Cancel",  // Button name
+        key: "Iframe",  // Button id (unique)
+        content: {
+          moduleName: "Iframe",
+          controlName: "Iframe"
+        },
+        class: "pwp-custom-btn"
+      }];
+
+      return items;
+    };
+
+    this.isEnabled = (itemKey) => {
+      return true;
+    };
+
+    this.onClick = function () {
+
+      const dialogs = require("core/dialogs");
+      const scope = $scope.$parent;
+
+      const items = scope.viewStats.selected_orders;
+
+      if (items.length === 0 || items.length > 1) {
+        dialogs.addNotify(items.length === 0 ? "Please select order before pressing the button." : "Please select only one order", "WARNING");
+        return;
+      }
+
+      const selectedOrderInfo = scope.viewStats.orders.find(elem => elem.OrderId === items[0].id);
+
+      console.log("selectedOrderInfo", selectedOrderInfo)
+      localStorage.setItem('move_cancel_selected_order_info', JSON.stringify(selectedOrderInfo));
+
+      var win = new wind({
+        moduleName: "Iframe",
+        windowName: "Iframe",
+        title: "Move/Cancel",
+        closeOnEscape: false,
+        closeOnBackDrop: false,
+        data: {
+          selectedOrderInfo
+        },
+        width: "800px",
+        height: "600px",
+        ngScope: $scope,
+      });
+
+      win.open();
+    };
+
+  }
+
+  placeholderManager.register("OpenOrders_OrderControlButtons", LookupPlaceholder);
+
+  $(document).ready(function ($scope, sessionManagerService) {
+
+  })
+});
